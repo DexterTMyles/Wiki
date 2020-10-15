de5c1fa5d0b015b8bb8e8227434cc35bf70f16cd
DexterTMyles <dextermyles18@yahoo.com>
2020-10-14 23:31:14 -0400

    Added entry Page

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", views.home, name="home")
+
+    path("wiki/<str:title>", views.entry, name="entry")
 
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,26 @@
 from django.shortcuts import render
-
+from markdown2 import markdown
 from . import util
 
 from django.http import HttpResponse
 
-
-def home(request):
-    return HttpResponse("HI")
-
-
+# Home Page
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+
+# Render a page that displays the contents of that encyclopedia entry.
+def entry(request, title):
+    page_content = util.get_entry(title)
+
+    if not page_content:
+        return render(request, "encyclopedia/error_page.html", {
+            "message": f"{title.capitalize()}, is not part of this Wiki page"
+        })
+
+    return render(request, "encyclopedia/entry.html",  {
+        "title": title.capitalize(),
+        "content": markdown(page_content)
     })
